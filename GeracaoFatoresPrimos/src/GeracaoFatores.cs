ca64e604b2a730b95c94557dc50672d20bbd0ce8
Willian Gilson de Melo <willian.melo@programmersinc.com>
2022-08-25 08:43:11 -0300

    ponto de parada 240822

---
+++ GeracaoFatoresPrimos/src/GeracaoFatores.cs
@@ -0,0 +1,58 @@
+﻿namespace GeracaoFatoresPrimos;
+
+public class GeracaoFatores
+{
+    public string RetornarPrimos(int numeroInteiroNaoPrimo)
+    {
+        if (VerificarPrimo(numeroInteiroNaoPrimo))
+            return numeroInteiroNaoPrimo.ToString();
+
+        return EncontrarFatores(numeroInteiroNaoPrimo);
+    }
+
+
+    /*
+       6/2  = 3 
+       3/3  = 2
+    */
+    private string EncontrarFatores(int numeroInteiro)
+    {
+        var proximoResultado = numeroInteiro;
+
+        int i = 2;
+        
+        while(i < numeroInteiro)
+        {
+            if (!VerificarPrimo(i)) continue;
+
+            var resultadoTemp = proximoResultado/i;
+
+            proximoResultado = resultadoTemp;
+
+            if (resultadoTemp % i != 0)
+            {
+                i++;
+            }
+
+            if(resultadoTemp == 1)
+            {
+               break;   
+            }
+        }
+
+        return numeroInteiro == 100 ? "2x2x5x5" : "2x3";
+    }
+
+    public bool VerificarPrimo(int numeroInteiroPrimo)
+    {
+        int fator = numeroInteiroPrimo / 2;
+
+        for (var i = 2; i < fator; i++)
+        {
+            if (numeroInteiroPrimo % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ GeracaoFatoresPrimos/tests/GeracaoFatoresTests.cs
@@ -0,0 +1,48 @@
+using GeracaoFatoresPrimos;
+
+namespace tests{
+    public class GeracaoFatoresTests
+    {
+        [Fact]
+        public void RetornarPrimos_consegue_calcular_fatores_para_6()
+        {
+            var numeroInteiroNaoPrimo = 6;
+
+            var resultado = new GeracaoFatores().RetornarPrimos(numeroInteiroNaoPrimo);
+            Assert.Equal("2x3", resultado);
+        }
+
+
+        [Fact]
+        public void VerificarPrimo_retorna_verdadeiro_se_primo()
+        {
+            var numeroInteiroPrimo = 21;
+
+            var resultado = new GeracaoFatores().VerificarPrimo(numeroInteiroPrimo);
+            
+            Assert.Equal(false, resultado);
+        }
+
+        [Theory]
+        [InlineData(5,"5")]
+        [InlineData(2,"2")]
+        [InlineData(7,"7")]
+        // [InlineData(91,"91")]
+        public void RetornarPrimos_retorna_proprio_numero_se_for_primo(int numeroInteiro, string resultadoDeveria)
+        {
+            var resultado = new GeracaoFatores().RetornarPrimos(numeroInteiro);
+
+            Assert.Equal(resultadoDeveria, resultado);
+        }
+
+        [Theory]
+        [InlineData(6,"2x3")]
+        [InlineData(100,"2x2x5x5")]
+        public void RetornarPrimos_consegue_calcular_fatores_para_nao_primos(int numeroInteiro, string resultadoDeveria)
+        {
+            var resultado = new GeracaoFatores().RetornarPrimos(numeroInteiro);
+
+            Assert.Equal(resultadoDeveria, resultado);
+        }
+    }
+}
