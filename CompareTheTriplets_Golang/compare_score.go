4e2b98d79826e0563039976fa04e60f1c1f5254e
Eduardo Pooter Reis <eduardo.reis@fire-engine-red.com>
2022-08-29 19:45:03 -0300

    Finished tests and solution.

--- CompareTheTriplets_Golang/compare_score.go
+++ CompareTheTriplets_Golang/compare_score.go
@@ -1,9 +1,16 @@
 package main
 
 func Comparador(a []int32, b []int32) []int32 {
+	var scoreA int32 = 0
+	var scoreB int32 = 0
 
-	// for i, x := 3 {
-	// 	total += x
-	// }
-	return []int32{3, 0}
+	for i := 0; i < 3; i++ {
+		if a[i] > b[i] {
+			scoreA++
+		} else if a[i] < b[i] {
+			scoreB++
+		}
+	}
+
+	return []int32{scoreA, scoreB}
 }
--- CompareTheTriplets_Golang/compare_score_test.go
+++ CompareTheTriplets_Golang/compare_score_test.go
@@ -24,3 +24,66 @@ func Test_AliceMelhorPontuacao(t *testing.T) {
 		}
 	}
 }
+
+func Test_AliceMenorPontuacao(t *testing.T) {
+	// Arrange
+	pontuacaoAlice := []int32{1, 1, 1}
+	pontuacaoBob := []int32{5, 5, 5}
+	esperado := []int32{0, 3}
+
+	// Act
+	resultado := Comparador(pontuacaoAlice, pontuacaoBob)
+
+	// Assert
+	for i := 0; i < 2; i++ {
+		if resultado[i] != esperado[i] {
+			t.Error(
+				"Pontuação", i,
+				"Esperado", esperado[i],
+				"Obteve", resultado[i],
+			)
+		}
+	}
+}
+
+func Test_AliceIgualPontuacao(t *testing.T) {
+	// Arrange
+	pontuacaoAlice := []int32{1, 97, 3}
+	pontuacaoBob := []int32{1, 97, 3}
+	esperado := []int32{0, 0}
+
+	// Act
+	resultado := Comparador(pontuacaoAlice, pontuacaoBob)
+
+	// Assert
+	for i := 0; i < 2; i++ {
+		if resultado[i] != esperado[i] {
+			t.Error(
+				"Pontuação", i,
+				"Esperado", esperado[i],
+				"Obteve", resultado[i],
+			)
+		}
+	}
+}
+
+func Test_AliceMaiorMisto(t *testing.T) {
+	// Arrange
+	pontuacaoAlice := []int32{2, 97, 3}
+	pontuacaoBob := []int32{1, 90, 5}
+	esperado := []int32{2, 1}
+
+	// Act
+	resultado := Comparador(pontuacaoAlice, pontuacaoBob)
+
+	// Assert
+	for i := 0; i < 2; i++ {
+		if resultado[i] != esperado[i] {
+			t.Error(
+				"Pontuação", i,
+				"Esperado", esperado[i],
+				"Obteve", resultado[i],
+			)
+		}
+	}
+}
