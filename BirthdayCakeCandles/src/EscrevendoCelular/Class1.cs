fda64aac2c00850fab9695b57c02d19dff94ca34
Willian Gilson de Melo <willian.melo@programmersinc.com>
2022-09-15 17:05:02 -0300

    ulitmo teste quebrando

---
+++ BirthdayCakeCandles/src/EscrevendoCelular/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace EscrevendoCelular;
+public class Calcular
+{
+    public int ObterMaior(List<int> velas)
+    {   
+        return velas.GroupBy(vela => vela).Where(vela => vela.Count() > 1).First().Count();
+    }
+}
