d40be0124800d192826f37f41a7a5aec340a199c
Willian Gilson de Melo <willian.melo@programmersinc.com>
2022-09-16 16:08:52 -0300

    finalizacao exercicio

--- BirthdayCakeCandles/src/EscrevendoCelular/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EscrevendoCelular;
-public class Calcular
-{
-    public int ObterMaior(List<int> velas)
-    {   
-        return velas.GroupBy(vela => vela).Where(vela => vela.Count() > 1).First().Count();
-    }
-}
