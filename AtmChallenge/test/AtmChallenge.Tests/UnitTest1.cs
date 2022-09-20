15ce129db43d9b85d191abc66c10070e208e8e6e
Willian Gilson de Melo <willian.melo@programmersinc.com>
2022-09-20 16:52:36 -0300

    Missing refactor

--- AtmChallenge/test/AtmChallenge.Tests/UnitTest1.cs
+++ AtmChallenge/test/AtmChallenge.Tests/UnitTest1.cs
@@ -1,10 +1,46 @@
-namespace EscrevendoCelular.Tests;
+using AtmChallenge;
+
+namespace AtmChallenge.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Withdrawl_WhenAskedTenReais_ReturnsOneNoteOfTenReais()
     {
+        var expected = new List<int>(){10};
 
+        var result = Atm.Withdraw(10);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Withdrawl_WhenAskedThirtyReais_ReturnsOneNoteOfTenReaisAndOneOfTwenty()
+    {
+        var expected = new List<int>{ 10, 20 };
+ 
+        var result = Atm.Withdraw(30);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Withdrawl_WhenAskedEightyReais_ReturnsOneNoteOfFiftyReaisAndOneOfTwentyAndOneNoteOfTen()
+    {
+        var expected = new List<int>{ 10, 20, 50 };
+ 
+        var result = Atm.Withdraw(80);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Withdrawl_WhenAskedHundredReais_ReturnsOneNoteOfHundredReais()
+    {
+        var expected = new List<int>{ 100 };
+ 
+        var result = Atm.Withdraw(100);
+
+        Assert.Equal(expected, result);
     }
 }
