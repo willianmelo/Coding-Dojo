9d510c65708a1c3c42d166ab43839f9317fb1fb4
Willian Gilson de Melo <willian.melo@programmersinc.com>
2022-09-23 10:57:32 -0300

    Withdrawl challenge

--- AtmChallenge/src/AtmChallenge/Atm.cs
+++ AtmChallenge/src/AtmChallenge/Atm.cs
@@ -1,34 +1,35 @@
 namespace AtmChallenge;
-public static class Atm
-{
-    public static List<int> Withdraw(int amount)
-    {
-        List<int> notes = new List<int>(); 
 
-        while (amount > 0)
+public class Atm
+{
+    private List<int> _availableNotes;
+    public Atm()
+    {
+        _availableNotes = new List<int> { 100, 50, 20, 10 };
+    }
+
+    public List<int> Withdraw(int amount)
+    {
+        var notes = new List<int>();
+
+        foreach (var note in _availableNotes)
         {
-            if (amount >= 100)
+            while (amount >= note) 
             {
-                amount = amount - 100;
-                notes.Add(100);
+                amount = AdjustBalance(notes, amount, note);
             }
-            else if (amount >= 50)
-            {
-                amount = amount - 50;
-                notes.Add(50);
-            }
-            else if (amount >= 20)
-            {
-                amount = amount - 20;
-                notes.Add(20);
-            }
-            else
-            {
-                amount = amount - 10;
-                notes.Add(10);
-            }
-         }
-        
+                
+            if (amount == 0) break;
+        }
+
         return notes.OrderBy(note => note).ToList();
+    }
+
+    private int AdjustBalance(IList<int> notes, int amount, int note)
+    {
+        amount -= note;
+        notes.Add(note);
+
+        return amount;
     }
 }
--- AtmChallenge/test/AtmChallenge.Tests/AtmTests.cs
+++ AtmChallenge/test/AtmChallenge.Tests/AtmTests.cs
@@ -1,15 +1,21 @@
+using System.Runtime.InteropServices.ComTypes;
 using AtmChallenge;
 
 namespace AtmChallenge.Tests;
 
-public class UnitTest1
+public class AtmTests
 {
+    private Atm _atm;
+    public AtmTests()
+    {
+        _atm = new Atm();
+    }
     [Fact]
     public void Withdrawl_WhenAskedTenReais_ReturnsOneNoteOfTenReais()
     {
         var expected = new List<int>(){10};
 
-        var result = Atm.Withdraw(10);
+        var result = _atm.Withdraw(10);
 
         Assert.Equal(expected, result);
     }
@@ -18,8 +24,8 @@ public class UnitTest1
     public void Withdrawl_WhenAskedThirtyReais_ReturnsOneNoteOfTenReaisAndOneOfTwenty()
     {
         var expected = new List<int>{ 10, 20 };
- 
-        var result = Atm.Withdraw(30);
+
+        var result = _atm.Withdraw(30);
 
         Assert.Equal(expected, result);
     }
@@ -28,8 +34,8 @@ public class UnitTest1
     public void Withdrawl_WhenAskedEightyReais_ReturnsOneNoteOfFiftyReaisAndOneOfTwentyAndOneNoteOfTen()
     {
         var expected = new List<int>{ 10, 20, 50 };
- 
-        var result = Atm.Withdraw(80);
+
+        var result = _atm.Withdraw(80);
 
         Assert.Equal(expected, result);
     }
@@ -38,8 +44,8 @@ public class UnitTest1
     public void Withdrawl_WhenAskedHundredReais_ReturnsOneNoteOfHundredReais()
     {
         var expected = new List<int>{ 100 };
- 
-        var result = Atm.Withdraw(100);
+
+        var result = _atm.Withdraw(100);
 
         Assert.Equal(expected, result);
     }
