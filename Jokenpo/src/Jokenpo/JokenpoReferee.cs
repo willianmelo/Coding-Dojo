cfb63a14147e5e0c5466fc7d710f8054e1ea0fe3
Willian Gilson de Melo <willian.melo@programmersinc.com>
2022-10-03 09:50:01 -0300

    Jokenpo challenge

---
+++ Jokenpo/src/Jokenpo/JokenpoReferee.cs
@@ -0,0 +1,15 @@
+﻿namespace Jokenpo;
+
+public static class JokenpoReferee
+{
+
+    public static string DetermineWinner(string gesture1, string gesture2) => 
+    ((gesture1, gesture2)) switch
+        {
+            ("rock", "paper") or ("paper", "rock") => "paper beats rock",
+            ("rock", "scissors") or ("scissors", "rock") => "rock beats scissors",
+            ("paper", "scissors") or ("scissors", "paper") => "scissors beats paper",
+            (_, _) => "draw"
+        };
+}
+
